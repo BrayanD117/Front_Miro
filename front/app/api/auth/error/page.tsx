97677611a0ff6a394ba2bb4c91d27a77522fc97a
Brayan Patiño <brayandavid1928@gmail.com>
2024-08-18 11:34:39 -0500

    Update UI in error page

--- front/app/api/auth/error/page.tsx
+++ front/app/api/auth/error/page.tsx
@@ -1,32 +1,41 @@
-'use client';
+"use client";
 
-import { useSearchParams, useRouter } from 'next/navigation';
-import { Container, Title, Text, Button, Group } from '@mantine/core';
-import { IconArrowLeft } from '@tabler/icons-react';
-import Lottie from 'lottie-react';
-import errorAnimation from '@/public/lottie/error.json';
+import { useSearchParams, useRouter } from "next/navigation";
+import { Container, Title, Text, Button, Group } from "@mantine/core";
+import { IconArrowLeft, IconBulb } from "@tabler/icons-react";
+import Lottie from "lottie-react";
+import errorAnimation from "@/public/lottie/error.json";
 
 export default function ErrorPage() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const error = searchParams.get('error');
+  const error = searchParams.get("error");
 
   return (
-    <Container style={{ textAlign: 'center', paddingTop: '5rem', maxWidth: '600px' }}>
-      <Lottie animationData={errorAnimation} loop={true} style={{ height: 200, marginBottom: '2rem' }} />
-      <Title order={1} style={{ fontSize: '2.5rem', marginBottom: '1rem' }}>
-        ¡Ups! Acceso Denegado
+    <Container
+      style={{ textAlign: "center", paddingTop: "5rem", maxWidth: "600px" }}
+    >
+      <Lottie
+        animationData={errorAnimation}
+        loop={false}
+        style={{ height: 200, marginBottom: "2rem" }}
+      />
+      <Title order={1} style={{ fontSize: "2.5rem", marginBottom: "1rem" }}>
+        ¡Acceso Denegado! 
       </Title>
-      <Text size="lg" color="dimmed" style={{ marginBottom: '1.5rem' }}>
+      <Text size="lg" color="dimmed" style={{ marginBottom: "1.5rem" }}>
         Parece que no tienes permiso para acceder a esta página.
       </Text>
-      <Text size="md" style={{ marginBottom: '2rem' }}>
-        {error && `Error: ${error}`}
+      <Text c="dimmed" size="xs" ta={"center"} mt="md" mb={"md"}>
+        <IconBulb color="#797979" size={20}></IconBulb>
+        <br />
+        Si crees que es un error, contacta al administrador del sistema.
       </Text>
       <Group justify="center">
         <Button
           size="md"
-          onClick={() => router.push('/')}
+          variant="light"
+          onClick={() => router.push("/")}
           leftSection={<IconArrowLeft size={16} />}
         >
           Volver al Inicio
