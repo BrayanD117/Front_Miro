59d993f730dc484d27ee0e63578eb54276c80c2e
Brayan Patiño <brayandavid1928@gmail.com>
2024-08-31 14:27:06 -0500

    Create ecosytem config

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+    apps: [
+      {
+        name: 'Front_Miro',
+        script: 'npm',
+        args: 'start',
+        env: {
+          NODE_ENV: 'production',
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+          NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+          GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+          GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+          NODE_TLS_REJECT_UNAUTHORIZED: process.env.NODE_TLS_REJECT_UNAUTHORIZED,
+        },
+      },
+    ],
+  };
+  
