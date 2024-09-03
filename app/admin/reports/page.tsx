b58bd9832e3dc78b95a00ead881733ca7abd73b6
Steven Correa <steven14correa@gmail.com>
2024-09-03 10:11:49 -0500

    Feature: Edit report complete

--- app/admin/reports/page.tsx
+++ app/admin/reports/page.tsx
@@ -19,6 +19,7 @@ import {
   Select,
   Tooltip,
   Title,
+  Pill,
 } from "@mantine/core";
 import {
   IconArrowRight,
@@ -145,7 +146,7 @@ const AdminReportsPage = () => {
   }, [search, session?.user?.email, page]);
 
   const handleCreateOrEdit = async () => {
-    if (!name || !reportExample) {
+    if ((!name || !reportExample) && !selectedReport) {
       showNotification({
         title: "Error",
         message: "El nombre y el formato de ejemplo son requeridos",
@@ -159,15 +160,16 @@ const AdminReportsPage = () => {
     formData.append("description", description);
     formData.append("requires_attachment", requiresAttachment.toString());
     formData.append("file_name", fileName);
-    formData.append("report_example", reportExample);
     formData.append("email", session?.user?.email || "");
+    if(reportExample)
+      formData.append("report_example", reportExample);
 
     setLoading(true);
 
     try {
       if (selectedReport) {
         await axios.put(
-          `${process.env.NEXT_PUBLIC_API_URL}/reports/${selectedReport._id}`,
+          `${process.env.NEXT_PUBLIC_API_URL}/reports/update/${selectedReport._id}`,
           formData,
           {
             headers: {
@@ -251,6 +253,7 @@ const AdminReportsPage = () => {
     setRequiresAttachment(false);
     setReportExample(null);
     setSelectedReport(null);
+    setFileName("");
     setLoading(false);
     setSuccess(false);
   };
@@ -490,6 +493,26 @@ const AdminReportsPage = () => {
                 }
               />
             </Group>
+            {selectedReport && (
+              <Group mt="sm">
+                <Text size="sm">
+                  Reporte cargado:{" "}
+                </Text>
+                <Pill
+                  onClick={() => {
+                    if (typeof window !== "undefined")
+                      window.open(selectedReport.report_example_link);
+                  }}
+                  style={{ cursor: "pointer" }}
+                  bg={"blue"}
+                  c={"white"}
+                  size="xs"
+                >
+                  {selectedReport.file_name}
+                </Pill>
+              </Group>
+            )}
+
           </>
         )}
       </Modal>
