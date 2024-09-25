2ad2f83ee138cc6f9539d3ab9fa399f870cfc27c
BrayanD117 <brayandavid1928@gmail.com>
2024-09-25 08:31:26 -0500

    Close #24, Add table with values

--- app/validations/page.tsx
+++ app/validations/page.tsx
@@ -12,24 +12,24 @@ import {
   Center,
   Divider,
   Box,
-  Group,
   Stack,
+  Table,
   Paper,
 } from '@mantine/core';
 import axios from 'axios';
-import { IconBulb } from '@tabler/icons-react';
+import { IconBulb, IconCopy } from '@tabler/icons-react';
 
 interface Validator {
   _id: string;
   name: string;
-  description: string;
+  description?: string;
   columns: Column[];
 }
 
 interface Column {
   name: string;
-  type: string;
   is_validator: boolean;
+  type: string;
   values: any[];
 }
 
@@ -38,20 +38,21 @@ const ValidationsPage = () => {
   const [selectedValidator, setSelectedValidator] = useState<Validator | null>(null);
   const [loading, setLoading] = useState(false);
 
+  const [columnNames, setColumnNames] = useState<string[]>([]);
+  const [tableRows, setTableRows] = useState<any[]>([]);
+
   useEffect(() => {
     const fetchValidators = async () => {
       try {
         const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/validators/allValidators`);
-        console.log("response.data.validators", response.data.validators);
         setValidators(response.data.validators);
       } catch (error) {
         console.error("Error al obtener las validaciones:", error);
       }
     };
-  
+
     fetchValidators();
   }, []);
-  
 
   const handleSelectValidator = async (id: string) => {
     setLoading(true);
@@ -59,7 +60,23 @@ const ValidationsPage = () => {
       const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/validators/id`, {
         params: { id },
       });
-      setSelectedValidator(response.data.validator);
+      const validator = response.data.validator;
+
+      const columnNames = validator.columns.map((col: Column) => col.name);
+      const numRows = validator.columns[0].values.length;
+
+      const rows = [];
+      for (let i = 0; i < numRows; i++) {
+        const row: any = {};
+        validator.columns.forEach((col: Column) => {
+          row[col.name] = col.values[i];
+        });
+        rows.push(row);
+      }
+
+      setSelectedValidator(validator);
+      setColumnNames(columnNames);
+      setTableRows(rows);
     } catch (error) {
       console.error("Error al obtener la validación:", error);
     } finally {
@@ -70,7 +87,7 @@ const ValidationsPage = () => {
   return (
     <Container size="xl">
       <Grid>
-        <Grid.Col span={3}>
+        <Grid.Col span={{ base: 12, md: 3}}>
             <Box>
               <Title m={5} ta={"center"}>Validaciones</Title>
               <Text c="dimmed" size="md" ta={"center"} m="md" >
@@ -92,36 +109,42 @@ const ValidationsPage = () => {
                         </Stack>
                     </ScrollArea>
                 </Paper>
-            </Box>
+              </Box>
         </Grid.Col>
         <Grid.Col span={9}>
           <div style={{ padding: '1rem' }}>
-          <Divider orientation="vertical" />
             {loading ? (
               <Center>
                 <Loader />
               </Center>
             ) : selectedValidator ? (
               <div>
-                <Title order={2}>{selectedValidator.name}</Title>
+                <Title ta={"center"} order={1}>{selectedValidator.name}</Title>
                 {selectedValidator.description && (
                   <Text mt="md">{selectedValidator.description}</Text>
                 )}
-                <Title order={4} mt="lg">Columnas:</Title>
-                {selectedValidator.columns.map((column, index) => (
-                  <div key={index}>
-                    <Text>
-                      <strong>Nombre:</strong> {column.name}
-                    </Text>
-                    <Text>
-                      <strong>Tipo:</strong> {column.type}
-                    </Text>
-                    <Text>
-                      <strong>Es validador:</strong> {column.is_validator ? 'Sí' : 'No'}
-                    </Text>
-                    <Divider my="sm" />
-                  </div>
-                ))}
+                <Title order={4} mt="lg">Datos de la Validación:</Title>
+                <ScrollArea>
+                  <Table mt={"md"} striped highlightOnHover withColumnBorders withTableBorder>
+                    <Table.Thead>
+                      <Table.Tr>
+                        {columnNames.map((name, index) => (
+                          <Table.Th key={index}>{name}</Table.Th>
+                        ))}
+                      </Table.Tr>
+                    </Table.Thead>
+                    <Table.Tbody>
+                      {tableRows.map((row, rowIndex) => (
+                        <Table.Tr key={rowIndex}>
+                          {columnNames.map((colName, colIndex) => (
+                            <Table.Td key={colIndex}>{row[colName]}</Table.Td>
+                          ))}
+                        </Table.Tr>
+                      ))}
+                    </Table.Tbody>
+                    <Table.Caption>Click sobre la validacion para copiar su valor. <br/> <IconCopy size={20}/> </Table.Caption>
+                  </Table>
+                </ScrollArea>
               </div>
             ) : (
               <Text>Selecciona una validación de la lista para ver sus detalles.</Text>
