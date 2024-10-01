1b1d7daee73886e55768f9e987b0d353dd12969f
Steven Correa <steven14correa@gmail.com>
2024-10-01 18:12:20 -0500

    Now checks the APP_ENV instead NODE_ENV

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 import dotenv from 'dotenv';
 
-console.log('NODE_ENV', process.env.NODE_ENV);
 
 dotenv.config();
 const nextConfig = {
-  basePath: process.env.NODE_ENV==='production' ? '' : '/dev',
+  basePath: process.env.APP_ENV==='production' ? '' : '/dev',
 };
 
 export default nextConfig;
