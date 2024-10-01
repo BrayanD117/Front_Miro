185406b50577187c9ad67e5f47aa73d2ef1e33ff
Steven Correa <steven14correa@gmail.com>
2024-10-01 18:08:32 -0500

    Test for checking env

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 import dotenv from 'dotenv';
 
+console.log('NODE_ENV', process.env.NODE_ENV);
+
 dotenv.config();
 const nextConfig = {
   basePath: process.env.NODE_ENV==='production' ? '' : '/dev',
