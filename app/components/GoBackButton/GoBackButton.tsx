bd7eb511ec4c5f87c64beaa25e5d4eaa4dd86829
BrayanD117 <brayandavid1928@gmail.com>
2025-01-24 08:02:23 -0500

    Update GoBackButton style

--- app/components/GoBackButton/GoBackButton.tsx
+++ app/components/GoBackButton/GoBackButton.tsx
@@ -4,16 +4,24 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
 import { Button } from "@mantine/core";
-import { IconArrowLeft, IconArrowLeftFromArc, IconArrowLeftTail } from "@tabler/icons-react";
+import { IconArrowLeft } from "@tabler/icons-react";
 
 const GoBackButton = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  if (pathname === "/" || pathname === "/dashboard" ) return null;
+  if (pathname === "/" || pathname === "/dashboard") return null;
 
   return (
-    <div style={{ position: "fixed", top: 63, left: 16, zIndex: 1000 }}>
+    <div
+      style={{
+        position: "fixed",
+        top: 63,
+        left: 16,
+        zIndex: 1000,
+        backdropFilter: "blur(10px)",
+      }}
+    >
       <Button
         onClick={() => router.back()}
         variant="light"
